801406a0155811c2bf5027fb8b4ba29b4e94d93a
Brecken Lusk <47198832+BreckenLusk@users.noreply.github.com>
2020-06-24 16:11:37 -0600

    Update ResetWindows.sh

--- ResetWindows.sh
+++ ResetWindows.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+
+# Deletes System Preferences' saved window position and restores it to default position.
+defaults delete ~/Library/Preferences/com.apple.systempreferences.plist "NSWindow Frame Main Window Frame SystemPreferencesApp 8.0"
+
+
+
+defaults delete com.apple.systempreferences.plist "NSWindow Frame Main Window Frame SystemPreferencesApp 8.0"
 defaults delete com.apple.Terminal "NSWindow Frame ${ProfileName}" # Reset the Terminal windows.
 defaults delete com.apple.Safari "NSWindow Frame BrowserWindowFrame" # Reset the Safari windows.
+
+defaults delete com.apple.Safari "NSWindow Frame BrowserWindowFrame" # Reset the Safari windows.
+
