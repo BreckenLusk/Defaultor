b49a6997cc2d7c8304452f1ff013d313723014ae
Brecken Lusk <47198832+BreckenLusk@users.noreply.github.com>
2020-06-24 16:12:01 -0600

    Update ResetWindows.sh

--- ResetWindows.sh
+++ ResetWindows.sh
@@ -5,6 +5,7 @@
 defaults delete ~/Library/Preferences/com.apple.systempreferences.plist "NSWindow Frame Main Window Frame SystemPreferencesApp 8.0"
 
 
+# Any code below this point is non funtional as of right now.
 
 defaults delete com.apple.systempreferences.plist "NSWindow Frame Main Window Frame SystemPreferencesApp 8.0"
 defaults delete com.apple.Terminal "NSWindow Frame ${ProfileName}" # Reset the Terminal windows.
