c216b900e5302627921713fbeb944c8427ebf8f2
Brecken Lusk <47198832+BreckenLusk@users.noreply.github.com>
2023-10-20 14:05:01 -0600

    Update ResetWindows.sh

--- ResetWindows.sh
+++ ResetWindows.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 
 
+#Tested/Working Apps:
+
+#Safari
+defaults delete com.apple.Safari 'NSWindow Frame BrowserWindowFrame'
+
+#Messages
+defaults delete com.apple.MobileSMS
+
+#FaceTime
+defaults delete com.apple.FaceTime
+
+#Contacts
+defaults delete com.apple.AddressBook
+
+#Activity Monitor
+defaults delete com.apple.ActivityMonitor 'NSWindow Frame main window'
+
+#Reminders
+defaults delete com.apple.Reminders
+
+#FindMy
+defaults delete com.apple.FindMy
+
+
+#Untested/Not Working Apps
+
+
+
+
 # Deletes System Preferences' saved window position and restores it to default position.
 defaults delete ~/Library/Preferences/com.apple.systempreferences.plist "NSWindow Frame Main Window Frame SystemPreferencesApp 8.0"
 
